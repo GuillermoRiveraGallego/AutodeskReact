6c56981b971fb6d8ae06b7103d4758c83201b31b
GuillermoRivera <guillermoriveragallego1999@gmail.com>
2025-06-27 09:05:10 +0200

    inicio

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div></div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button>count is</button>
+        <p>Edit and save to test HMR</p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
